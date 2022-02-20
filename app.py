ece488acd086a0e0160c79ec2ae2dcfa211a10db
SidDas8 <siddharthdas168@gmail.com>
2022-02-19 16:07:44 -0800

    Completed data base setup.

--- app.py
+++ app.py
@@ -6,3 +6,24 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+
+# Setting up database
+
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect = True)
+
+# View all of the classes that automap found
+Base.classes.keys()
+
+# Save references to each table
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
